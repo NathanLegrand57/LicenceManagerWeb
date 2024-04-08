acabe169bdb39d204d814d93223a8343064b664f
Nathan <nathanlgd14@gmail.com>
2024-04-08 16:43:06 +0200

    Avancement responsive page index licenceChoisie

--- resources/views/licence-choisie/index.blade.php
+++ resources/views/licence-choisie/index.blade.php
@@ -1,19 +1,19 @@
 <x-app-layout>
-    <p class="flex justify-end mt-10 mr-20">
-        <a class="bg-green-600 hover:bg-green-700 duration-500 text-white font-bold py-1 px-2 rounded"
+    <p class="flex justify-center sm:justify-end mt-4 sm:mt-10 sm:mr-20">
+        <a class="bg-green-600 hover:bg-green-700 duration-500 text-white text-sm min-[475px]:text-base font-bold py-1 px-2 rounded"
             href="{{ route('licence.index') }}">Demander une licence</a>
     </p>
-    <h1 class="text-3xl font-semibold text-center mb-10">Mes licences</h1>
-    <section class="w-fit mx-auto grid grid-cols-1 md:grid-cols-2 justify-items-center gap-y-10 gap-x-10 mt-10 mb-20">
+    <h1 class="text-xl min-[475px]:text-2xl sm:text-3xl font-semibold text-center mb-0 sm:mb-10 mt-4 sm:mt-5">Mes licences</h1>
+    <section class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-2 justify-items-center gap-y-2 sm:gap-y-10 gap-x-10 mt-0 sm:mt-10 mb-20">
         @forelse ($licences_choisies as $licence_choisie)
-            <div class="bg-white p-6 mt-6 rounded-lg shadow-md leading-8">
-                <h5 class="text-xl font-semibold mb-2">
+            <div class="bg-white text-sm min-[475px]:text-base sm:text-lg p-6 mt-6 rounded-lg shadow-md leading-6 min-[475px]:leading-8">
+                <h5 class="text-base min-[475px]:text-lg sm:text-xl font-semibold mb-2">
                     {{ $licence_choisie->licence->libelle }}</h5>
 
-                <p class="text-gray-700 text-lg"><strong>Début de la souscription :</strong>
+                <p class="text-gray-700"><strong>Début de la souscription :</strong>
                     {{ \Carbon\Carbon::parse($licence_choisie->date_debut)->format('d/m/Y') }}</p>
 
-                <p class="text-gray-700 text-lg"><strong>Fin de la souscription :</strong>
+                <p class="text-gray-700"><strong>Fin de la souscription :</strong>
                     {{ \Carbon\Carbon::parse($licence_choisie->date_fin)->format('d/m/Y') }}</p>
 
                 @if ($licence_choisie->jours_restants <= 0)
@@ -22,11 +22,11 @@
                         method="POST">
                         @csrf
                         <button type="submit"
-                            class="bg-blue-500 hover:bg-blue-700 duration-500 text-white font-bold py-0 px-2 rounded mt-6">Renouveler</button>
+                            class="bg-blue-500 hover:bg-blue-700 duration-500 text-white text-sm min-[475px]:text-base sm:text-lg  font-bold py-0 px-2 rounded mt-0 sm:mt-6">Renouveler</button>
                     </form>
 
                 @elseif ($licence_choisie->jours_restants <= 5)
-                    <p class="text-gray-700 text-lg"><strong>Jours restants avant expiration de la licence :</strong>
+                    <p class="text-gray-700"><strong>Jours restants avant expiration de la licence :</strong>
                         <span class="text-red-500">{{ $licence_choisie->jours_restants }}</span>
                     </p>
                     <p class="text-red-500"><strong>Attention ! La licence expire bientôt.</strong></p>
@@ -34,11 +34,11 @@
                         method="POST">
                         @csrf
                         <button type="submit"
-                            class="bg-blue-500 hover:bg-blue-700 duration-500 text-white font-bold py-0 px-2 rounded mt-6">Renouveler</button>
+                            class="bg-blue-500 hover:bg-blue-700 duration-500 text-white font-bold py-0 px-2 rounded mt-2 sm:mt-6">Renouveler</button>
                     </form>
 
                 @else
-                    <p class="text-gray-700 text-lg"><strong>Jours restants avant expiration de la licence :</strong>
+                    <p class="text-gray-700"><strong>Jours restants avant expiration de la licence :</strong>
                         {{ $licence_choisie->jours_restants }}</p>
                 @endif
 
